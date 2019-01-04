6ad0c8632772b69318838f41c75d5ac496518d93
Jaroslav Ištok <jaroslav.istok@gmail.com>
2019-01-04 12:07:01 +0100

    added create table statements

--- du_datove_sklady.sql
+++ du_datove_sklady.sql
@@ -23,57 +23,99 @@ create table time_dim(
 
 -- SCD2 (add new row)
 create table product_dim(
-
+    sku varchar2(50),
+    product_name varchar2(255),
+    brand_name varchar2(255),
+    category varchar2(255),
+    sub_category varchar2(255),
+    package_type varchar2(255),
+    product_weight varchar2(255)
 );
 
 -- SCD2 (add new row)
 create table store_dim(
-
+    store_name varchar2(255),
+    street varchar2(255),
+    city varchar2(255),
+    country varchar2(255),
+    postal_number varchar2(10),
+    area number,
+    store_type varchar2(255),
+    opened date
 );
 
 -- SCD2 (add new row)
 create table promotion_dim(
-
+    promotion_name varchar2(255),
+    promotion_start_date date, 
+    promotion_end_date date, 
+    advert_type varchar2(255),
+    coupon_type varchar2(255),
+    promotion_medium varchar2(255)
 );
 
 -- SCD2 (add new row)
 create table cashier_dim(
-
+    cashier_code varchar2(100),
+    cashier_forename varchar2(100),
+    cashier_surname varchar2(100),
 );
 
 -- SCD2 (add new row)
 create table payment_method_dim(
-
+    method varchar2(255)
 );
 
 -- Degenerated dimension
 create table transaction_code_dim(
-
+    transaction_code varchar2(255)
 );
 
 -- SCD 5 or SCD 6
 create table customers_dim(
-
+    customer_forename varchar2(255),
+    customer_surname varchar2(255),
+    customer_street varchar2(255),
+    customer_city varchar2(255),
+    customer_country varchar2(255),
+    customer_postal_code varchar2(255)
 );
 
 -- SCD2
 create table currency_dim(
-
+    currency varchar2(5)
 );
 
 
 -- Fact table
 create table sales_fact(
+    quantity number,
+    quantity_type varchar2(10),
+    unit_standart_price number, 
+    unit_costs number,
+    unit_discount number,
+    unit_price_after_discount number,
 
+    -- derivated facts
+    overall_standart_price number,
+    overall_discount number,
+    overall_costs_after_discount number,
+    overall_costs number,
+    profit number
+
+    currency varchar2(5),
+    special_discount number
 );
 
 -- Fact table
 create table payment_fact(
-
+    payed_price number,
+    currency varchar2(5)
 );
 
 -- Fact table
 create table points_fact(
-
+    used_points number,
+    gained_points number
 );
 
